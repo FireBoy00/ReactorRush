6acd0b823714bc32e4f0ba73e7db9f255dd86220
FireBoy00 <ancryson@gmail.com>
2024-12-29 13:14:00 +0200

    Add minigame functionality to VisitorCenter room

--- ReactorRush/rooms/VisitorCenter.cs
+++ ReactorRush/rooms/VisitorCenter.cs
@@ -1,12 +1,14 @@
 using System;
 using Spectre.Console;
 using ReactorRush;
+using Minigames;
 
 namespace Rooms
 {
     public class VisitorCenter : IRooms
     {
         private int score = 0;
+        private readonly List<IMinigame> minigames = new List<IMinigame>();
 
         public int StartLevel() {
             AnsiConsole.Clear();
@@ -15,7 +17,7 @@ namespace Rooms
             var welcomeMsg = "Welcome to the Power Room. Your task is to fix the power cables and restore power to the reactor. Good luck!Welcome to the Power Room. Your task is to fix the power cables and restore power to the reactor. Good luck!Welcome to the Power Room. Your task is to fix...Welcome to the Power Room. Your task is to fix the power cables and restore power to the reactor. Good luck!Welcome to the Power Room. Your task is to fix the power cables and restore power to the reactor. Good luck!Welcome to the Power Room. Your task is to fix...";
             Utility.PrintStory(welcomeMsg);
             Utility.PrintStory("Test concluded!", "FireBoy");
-            // minigames[1].Run();
+            minigames[1].Run();
             string prompt1 = Utility.Prompt("What's your name?", ["I don't know my name...", "In case of any anomalies or alarms, operators quickly diagnose and address the issues to prevent potential hazards.", "They adjust control rods and other mechanisms to manage the reactor's power output and maintain stability.", "The control room team coordinates with other plant personnel to manage routine operations and maintenance activities."]);
             AnsiConsole.Clear();
             AnsiConsole.Write($"You chose: {prompt1}");
