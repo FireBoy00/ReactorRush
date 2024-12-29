373518ddf1fb7f5a8e4a11bd49d058dba6d6c964
FireBoy00 <ancryson@gmail.com>
2024-12-29 12:40:45 +0200

    Improve console output formatting by adding new lines for better readability - Gabi

--- ReactorRush/Program.cs
+++ ReactorRush/Program.cs
@@ -9,10 +9,10 @@ namespace ReactorRush
     {
         static void Main(string[] args)
         {
-            AnsiConsole.Write(new Markup("[bold yellow]Note:[/] [red]Please maximize the window before running the game.[/]").Centered());
-            AnsiConsole.Write(new Markup("This will ensure that you have a full-screen experience.").Centered());
+            AnsiConsole.Write(new Markup("\n[bold yellow]Note:[/] [red]Please maximize the window before running the game.[/]").Centered());
+            AnsiConsole.Write(new Markup("\nThis will ensure that you have a full-screen experience.").Centered());
             Console.WriteLine();
-            AnsiConsole.Write(new Markup("If you're ready, press any key...").Centered());
+            AnsiConsole.Write(new Markup("\nIf you're ready, press any key...").Centered());
             Console.WriteLine();
             Console.ReadKey();
             Console.Clear();
