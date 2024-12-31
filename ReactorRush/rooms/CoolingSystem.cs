b2226b1c74eecec1a5e907e1013a41e8e72e755d
FireBoy00 <adrians.zorro@gmail.com>
2024-12-31 14:17:06 +0100

    Clear console before running minigame in CoolingSystem.cs

--- ReactorRush/rooms/CoolingSystem.cs
+++ ReactorRush/rooms/CoolingSystem.cs
@@ -16,6 +16,8 @@ namespace Rooms
             Utility.Narrator = "Operator";
             var welcomeMsg = "Cooling System room - the hero of the nuclear reactor! This is where operators manage heat transfer, keep coolant flowing smoothly, and ensure the pipes and pumps are in perfect shape. A working cooling system is what keeps the reactor safe and prevents any overheating disasters.\nUh-oh! We have a problem.\nOne of the pipes underneath the Containment building has cracked a leak, and it’s up to you to fix it before things get out of the control. Time to roll up your sleeves and get to work!";
             Utility.PrintStory(welcomeMsg);
+
+            AnsiConsole.Clear();
             minigames[4].Run();
  
             int NumberOfTries = 1;
