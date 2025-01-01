11bb11b5f12022f62306dee9348c9c5d625175ec
Paul <donicipaul1@gmail.com>
2025-01-01 22:15:48 +0100

    Room WaterReservoirs.cs is done(outside of minigame)

--- ReactorRush/rooms/WaterReservoirs.cs
+++ ReactorRush/rooms/WaterReservoirs.cs
@@ -18,9 +18,29 @@ namespace Rooms
             Utility.PrintStory("This room plays a critical role in ensuring the reactor operates efficiently and safely. Water is a vital component in the cooling and energy generation process of a nuclear power plant, and here, you will learn how this system works and why maintaining it is essential. ");
             Utility.PrintStory("I think this is the right time to talk about its purpose, what is it?");
             Utility.PrintStory("The Water Reservoir is a key part of the plant’s secondary cooling system. It stores and supplies water that condenses steam discharged from the turbines. This ensures a continues cycle of steam generation and condensation, which is crucial for maintaining the efficiency and safety of the plant.");
-            string prompt1 = Utility.Prompt("Did you know that the Water Reservoir helps prevent potential hazards?", ["Yes, tell me more", "No, I never thought or saw anything about it"]);
+            string prompt12 = Utility.Prompt("Did you know that the Water Reservoir helps prevent potential hazards?", ["Yes, tell me more", "No, I never thought or saw anything about it"]);
             Utility.PrintStory("It condenses any steam that might escape, avoiding pressure buildup and ensuring that hydrogen does not mix with oxygen to create an explosive environment. A nitrogen atmosphere in the reservoir further safeguards against it.");
-
+            Utility.PrintStory("What exactly happens in the room? ");
+            Utility.PrintStory("Firstly, steam exiting the turbines is cooled in the condenser and returned to water, which collects in the reservoir for reuse. Then the water is purified to remove impurities, protecting the steam generator and ensuring efficient heat transfer. And finally heaters regulate the water's temperature, optimizing plant efficiency. ");
+            Utility.PrintStory("We have some questions for you about this reservoir, do you think you could answer them correctly? ");
+            do {
+            prompt12 = Utility.Prompt("Why is it essential to remove impurities from the water in the reservoir before it enters the steam generator?", ["To prevent steam explosion risks.","To reduce heat transfer efficiency.", "To avoid damage to the steam generator and maintain efficiency.", "To lower the water pressure in the system."]);
+            
+           
+            if(prompt12 == "To prevent steam explosion risks.")
+                Utility.PrintStory("Incorrect! Try again.");
+                else
+            if(prompt12 == "To reduce heat transfer efficiency.")
+                Utility.PrintStory("Incorrect! Try again.");
+                else
+            if(prompt12 == "To avoid damage to the steam generator and maintain efficiency.")
+                Utility.PrintStory("You chose the right answer, procced with your given task. Good luck!");
+                else
+            if(prompt12 == "To lower the water pressure in the system.")
+                Utility.PrintStory("Incorrect! Try again.");
+            }while(prompt12 != "To avoid damage to the steam generator and maintain efficiency." );
+            Utility.PrintStory("You are faced with a practical challenge: a sudden swing in water levels! The system alerts you to correct the water level to maintain stability. ");
+            Utility.PrintStory("Your task is to adjust valves to stabilize the water levels. Watch the indicators closely and react quickly to prevent overflows or shortages. ");
             //? Minigame minigames[2].Run();
             AnsiConsole.Clear();
             player.UpdateRoomStatus(this.GetType().Name, Score > 0); // Update the room status
