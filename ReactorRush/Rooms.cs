0afa74a9d633e0f04034b550a9fb3850ec799848
FireBoy00 <adrians.zorro@gmail.com>
2025-01-01 20:25:02 +0100

    Reorder room initialization in Rooms class for correct indexing

--- ReactorRush/Rooms.cs
+++ ReactorRush/Rooms.cs
@@ -12,12 +12,12 @@ namespace Rooms
     {
         public static List<IRooms> Rooms { get; } = new List<IRooms>
         {
-            new VisitorCenter(), // 0
-            new CoolingSystem(), // 1
-            new FuelHandlingArea(), // 2
-            new WasteStorageFacility(), // 6
-            new RadiationMonitor(), // 11
-            new Laboratory() // 12
+            new VisitorCenter(), // 1
+            new CoolingSystem(), // 3
+            new WasteStorageFacility(), // 5
+            new FuelHandlingArea(), // 7
+            new RadiationMonitor(), // 10
+            new Laboratory() // 11
             // Add other rooms here
         };
     }
