8e7722827a5f318a2eedfbde29222156e243838d
gabija <gasta24@student.sdu.dk>
2025-01-01 16:26:47 +0100

    added my rooms

--- ReactorRush/Rooms.cs
+++ ReactorRush/Rooms.cs
@@ -9,7 +9,10 @@ namespace Rooms
     {
         public static List<IRooms> Rooms { get; } = new List<IRooms>
         {
-            new VisitorCenter() // 0
+            new VisitorCenter(), // 0
+            new WasteStorageFacility(), //4
+            new RadiationMonitor(), //9
+            new Laboratory() // 10
             // Add other rooms here
         };
     }
