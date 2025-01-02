3041a4e3d79b29bf21b0486278da56fe9096ef8e
FireBoy00 <adrians.zorro@gmail.com>
2025-01-02 16:50:40 +0100

    Remove unused minigameIndex variable and clean up RadiationMonitor room description

--- ReactorRush/rooms/Laboratory.cs
+++ ReactorRush/rooms/Laboratory.cs
@@ -8,7 +8,6 @@ namespace Rooms
     public class Laboratory : IRooms
     {
         public int Score { get; private set; }
-        private readonly int minigameIndex = -1; // Index of the minigame in the minigames list
         private readonly List<IMinigame> minigames = MinigameList.Minigames;
 
         public int StartLevel(Player player)
